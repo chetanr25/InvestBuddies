39dac22b71b835e9e68ecff9e0b2e41b2ef653e3
chetanr25 <1ds22ai010@dsce.edu.com>
2025-01-11 16:30:29 +0530

    Added firestore implementation

--- flutter_app/lib/providers/users_providers.dart
+++ flutter_app/lib/providers/users_providers.dart
@@ -8,10 +8,43 @@ final userProvider = StateNotifierProvider<UserNotifier, UserModel>((ref) {
 });
 
 class UserNotifier extends StateNotifier<UserModel> {
-  UserNotifier() : super(UserModel(profileCompleted: false));
-}
-
-class UserRepository {
+  UserNotifier()
+      : super(UserModel(
+          userId: '',
+          email: '',
+          displayName: '',
+          profileCompleted: false,
+          role: '',
+        )) {
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user != null) {
+        _loadUserData(user.uid);
+      } else {
+        state = UserModel(
+          userId: '',
+          email: '',
+          displayName: '',
+          profileCompleted: false,
+          role: '',
+        );
+      }
+    });
+  }
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<void> _loadUserData(String uid) async {
+    try {
+      final docSnapshot = await _firestore.collection('users').doc(uid).get();
+      if (docSnapshot.exists) {
+        final data = docSnapshot.data()!;
+        state = UserModel(
+          profileCompleted: data['profileCompleted'],
+          additionalData: data['additionalData'],
+        );
+      }
+    } catch (e) {
+      print('Error loading user data: $e');
+    }
+  }
 }
