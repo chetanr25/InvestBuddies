b374f7c8f451208e6a663e112f66a8198b5cf01c
chetanr25 <1ds22ai010@dsce.edu.com>
2025-01-11 16:29:25 +0530

    initilized providers for statemanagement of users, send data to firebase with proper error handling

--- flutter_app/lib/providers/users_providers.dart
+++ flutter_app/lib/providers/users_providers.dart
@@ -1,5 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_app/models/users_models.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final userProvider = StateNotifierProvider<UserNotifier, UserModel>((ref) {
   return UserNotifier();
 });
+
+class UserNotifier extends StateNotifier<UserModel> {
+  UserNotifier() : super(UserModel(profileCompleted: false));
+}
+
+class UserRepository {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+}
