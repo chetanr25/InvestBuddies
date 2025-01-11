23142c2464455563315bd60762fac01caae497f5
chetanr25 <1ds22ai010@dsce.edu.com>
2025-01-11 14:59:18 +0530

    fix: Ensure Firebase meets minimum iOS requirements for seamless integration and issue-free functionality

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -10,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Flutter Demo', home: const HomeScreen());
+    return MaterialApp(title: 'Invest Buddies', home: const HomeScreen());
   }
 }
