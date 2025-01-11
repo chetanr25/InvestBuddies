bbfe76673118188dc5bff620e865aa51b81a5501
chetanr25 <1ds22ai010@dsce.edu.com>
2025-01-11 22:08:49 +0530

    Added Constant Data for Registration details input

---
+++ flutter_app/lib/constants/registration_consts.dart
@@ -0,0 +1,107 @@
+const List<String> shortTermGoals = [
+  'Save for a vacation',
+  'Pay off debt',
+  'Emergency fund'
+];
+
+const List<String> mediumTermGoals = [
+  'Save for a house',
+  'Start a business',
+  'Education fund'
+];
+
+const List<String> longTermGoals = [
+  'Retirement planning',
+  'Wealth accumulation',
+  'Legacy and estate planning'
+];
+
+const List<String> ageGroups = [
+  'Under 18',
+  '18-25',
+  '26-35',
+  '36-50',
+  '51-65',
+  '65+'
+];
+
+const List<String> employmentStatuses = [
+  'Student',
+  'Employed',
+  'Self-employed',
+  'Retired',
+  'Unemployed'
+];
+
+const List<String> incomeRanges = [
+  'Less than ₹25,000',
+  '₹25,000-₹50,000',
+  '₹50,000-₹100,000',
+  'More than ₹100,000'
+];
+
+const List<String> industries = [
+  'Technology',
+  'Healthcare',
+  'Finance',
+  'Real Estate',
+  'Renewable Energy',
+  'Consumer Goods',
+  'Entertainment',
+  'Education'
+];
+
+const List<String> investmentInterests = [
+  'Stock Market Investing',
+  'Real Estate Investing',
+  'Cryptocurrency',
+  'Budgeting and Expense Tracking',
+  'Sustainable Investments',
+  'Retirement Accounts',
+  'Side Hustles',
+  'Entrepreneurship'
+];
+
+const List<String> riskLevels = [
+  'Low Risk - Savings accounts, bonds',
+  'Moderate Risk - Mutual funds, ETFs',
+  'High Risk - Stocks, cryptocurrencies, startups'
+];
+
+const List<String> updateFrequency = [
+  'Weekly updates',
+  'Monthly summaries',
+  'On-demand'
+];
+
+// const List<String> financialChallenges = [
+//   'High Debt',
+//   'Lack of Savings',
+//   'Investment Confusion',
+//   'Poor Credit Score',
+//   'Retirement Concerns',
+//   'Tax Efficiency'
+// ];
+
+const List<String> spendingHabits = ['Frugal', 'Moderate', 'Extravagant'];
+
+const List<String> financialKnowledge = [
+  'Beginner',
+  'Intermediate',
+  'Advanced'
+];
+
+const List<String> lifestylePreferences = [
+  'Minimalist',
+  'Comfortable',
+  'Luxurious'
+];
+
+const List<String> futurePlans = [
+  'Marriage',
+  'Buying a Home',
+  'Starting a Family',
+  'Changing Jobs',
+  'Relocation',
+  'Retirement'
+];
